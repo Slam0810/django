faa879b4e9bfb8094f6302c521c78230f6b65978
Birama0810 <birama.tour@gmail.com>
2021-03-10 23:06:49 +0100

    config in wsgi.py file an adding main.py file -> roolback

--- geams_sarl/wsgi.py
+++ geams_sarl/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'salazare.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geams_sarl.settings')
 
 application = get_wsgi_application()
